5b3fba041cb6087300454a11f9bee5db27eaba51
Gabriel Oliver <gabrielmed2010@hotmail.com>
2021-06-26 11:51:46 -0300

    feat: add the home page and the first attempt of colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+
 import "./App.css";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
